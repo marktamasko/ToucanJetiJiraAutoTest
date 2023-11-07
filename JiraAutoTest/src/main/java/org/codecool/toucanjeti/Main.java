3ab920ebf89b4f43fb45da7f6b0310b7d7c533ae
csege <cseloteigergely@gmail.com>
2023-11-07 13:47:48 +0100

    add close method to LoginTest, refactor LoginTest for encapsulation

--- JiraAutoTest/src/main/java/org/codecool/toucanjeti/Main.java
+++ JiraAutoTest/src/main/java/org/codecool/toucanjeti/Main.java
@@ -6,10 +6,6 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Main {
     public static void main(String[] args) {
         LoginTest loginTest = new LoginTest(new ChromeDriver());
-        loginTest.goToLink();
-//        loginTest.successfulLogin();
-//        loginTest.emptyFieldsLogin();
-//        loginTest.wrongUsernamePasswordFieldsLogin();
-        loginTest.legitUsernameWrongPasswordFieldsLogin();
+        loginTest.runTests();
     }
 }
