e93e66fcf6d927900642ecb358ec8895dc500896
csege <cseloteigergely@gmail.com>
2023-11-07 13:55:24 +0100

    correct runTests method in LoginTest

--- JiraAutoTest/src/main/java/org/codecool/toucanjeti/Main.java
+++ JiraAutoTest/src/main/java/org/codecool/toucanjeti/Main.java
@@ -2,6 +2,7 @@ package org.codecool.toucanjeti;
 
 import org.codecool.toucanjeti.Login.LoginTest;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Main {
     public static void main(String[] args) {
