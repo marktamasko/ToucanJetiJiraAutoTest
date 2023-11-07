4e752df00c3db6b60eeff9171fac7e011e06841a
csege <cseloteigergely@gmail.com>
2023-11-07 11:33:31 +0100

    modify folder architecture, add successfulLogin to LoginTest

---
+++ JiraAutoTest/src/main/java/org/codecool/toucanjeti/Login/LoginTest.java
@@ -0,0 +1,36 @@
+package org.codecool.toucanjeti.Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginTest {
+    private WebDriver driver;
+    private static final String LEGIT_USERNAME = "automation58";
+    private static final String LEGIT_PASSWORD = "CCAutoTest19.";
+    private static final String INVALID_USERNAME = "KISKUTYA";
+    private static final String INVALID_PASSWORD = "KISEBBKUTYA";
+    private static final String URL = "https://jira-auto.codecool.metastage.net/login.jsp";
+    private static final By USERNAME_INPUT_FIELD_ID = By.id("login-form-username");
+    private static final By PASSWORD_INPUT_FIELD_ID = By.id("login-form-password");
+    private static final By LOGIN_BUTTON_ID = By.id("login-form-submit");
+    private static final By AVATAR_ID = By.id("header-details-user-fullname");
+    private static final By PROFILE_ID = By.id("view_profile");
+    private static final By PROFILE_USERNAME_FIELD_ID = By.id("up-d-username");
+
+    public LoginTest(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void goToLink() {
+        driver.get(URL);
+    }
+    public void successfulLogin() {
+        driver.findElement(USERNAME_INPUT_FIELD_ID).sendKeys(LEGIT_USERNAME);
+        driver.findElement(PASSWORD_INPUT_FIELD_ID).sendKeys(LEGIT_PASSWORD);
+        driver.findElement(LOGIN_BUTTON_ID).click();
+        driver.findElement(AVATAR_ID).click();
+        driver.findElement(PROFILE_ID).click();
+        System.out.println("Logged in the right account: " +
+                driver.findElement(PROFILE_USERNAME_FIELD_ID).getText().equals(LEGIT_USERNAME));
+    }
+}
---
+++ JiraAutoTest/src/main/java/org/codecool/toucanjeti/Main.java
@@ -0,0 +1,12 @@
+package org.codecool.toucanjeti;
+
+import org.codecool.toucanjeti.Login.LoginTest;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        LoginTest loginTest = new LoginTest(new ChromeDriver());
+        loginTest.goToLink();
+        loginTest.successfulLogin();
+    }
+}
