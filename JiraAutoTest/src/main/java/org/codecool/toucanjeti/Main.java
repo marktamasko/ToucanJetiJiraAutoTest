4954b7eb986c4edc8c4efcec4d33335e8bde2a42
arpdkvcs <arpdkvcs@gmail.com>
2023-11-08 09:20:19 +0100

    Extract driver in main

--- JiraAutoTest/src/main/java/org/codecool/toucanjeti/Main.java
+++ JiraAutoTest/src/main/java/org/codecool/toucanjeti/Main.java
@@ -2,12 +2,15 @@ package org.codecool.toucanjeti;
 
 import org.codecool.toucanjeti.Login.LoginTest;
 import org.codecool.toucanjeti.Logout.LogoutTest;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chromium.ChromiumDriver;
 
 public class Main {
     public static void main(String[] args) {
-        LoginTest loginTest = new LoginTest(new ChromeDriver());
-        LogoutTest logoutTest = new LogoutTest(new ChromeDriver());
+        WebDriver driver = new ChromeDriver();
+        LoginTest loginTest = new LoginTest(driver);
+        LogoutTest logoutTest = new LogoutTest(driver);
         loginTest.runTests();
         logoutTest.logout();
     }
