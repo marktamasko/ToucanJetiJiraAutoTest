054635e73ca5e548cbf685e31616ef20516b89e0
arpdkvcs <arpdkvcs@gmail.com>
2023-11-08 10:54:56 +0100

    Refactor LogoutTest to use Login class for login

--- src/main/java/org/codecool/toucanjeti/Logout/LogoutTest.java
+++ src/main/java/org/codecool/toucanjeti/Logout/LogoutTest.java
@@ -1,5 +1,6 @@
 package org.codecool.toucanjeti.Logout;
 
+import org.codecool.toucanjeti.technical.Login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
@@ -7,10 +8,6 @@ public class LogoutTest {
     private WebDriver driver;
     private static final String LEGIT_USERNAME = "automation58";
     private static final String LEGIT_PASSWORD = "CCAutoTest19.";
-    private static final String URL = "https://jira-auto.codecool.metastage.net/login.jsp";
-    private static final By USERNAME_INPUT_FIELD_ID = By.id("login-form-username");
-    private static final By PASSWORD_INPUT_FIELD_ID = By.id("login-form-password");
-    private static final By LOGIN_BUTTON_ID = By.id("login-form-submit");
     private static final By AVATAR_ID = By.id("header-details-user-fullname");
     private static final By LOGOUT_ID = By.id("log_out");
 
@@ -18,10 +15,7 @@ public class LogoutTest {
         this.driver = driver;
     }
     public void logout() {
-        driver.get(URL);
-        driver.findElement(USERNAME_INPUT_FIELD_ID).sendKeys(LEGIT_USERNAME);
-        driver.findElement(PASSWORD_INPUT_FIELD_ID).sendKeys(LEGIT_PASSWORD);
-        driver.findElement(LOGIN_BUTTON_ID).click();
+        Login.toJiraWith(LEGIT_USERNAME, LEGIT_PASSWORD, driver);
         driver.findElement(AVATAR_ID).click();
         driver.findElement(LOGOUT_ID).click();
     }
