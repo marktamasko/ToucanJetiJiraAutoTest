467684174cf12da9a0cd3924d4b1fcfbb40f10aa
marktamasko <25462245+marktamasko@users.noreply.github.com>
2023-11-09 13:38:23 +0100

    feat[Main]: run all testcases are passed

--- src/main/java/org/codecool/toucanjeti/Main.java
+++ src/main/java/org/codecool/toucanjeti/Main.java
@@ -2,16 +2,27 @@ package org.codecool.toucanjeti;
 
 import org.codecool.toucanjeti.Login.LoginTest;
 import org.codecool.toucanjeti.Logout.LogoutTest;
+import org.codecool.toucanjeti.editIssue.EditIssue;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chromium.ChromiumDriver;
 
 public class Main {
     public static void main(String[] args) {
+
         WebDriver driver = new ChromeDriver();
         LoginTest loginTest = new LoginTest(driver);
         LogoutTest logoutTest = new LogoutTest(driver);
+        EditIssue editIssue = new EditIssue(driver);
+
         loginTest.runTests();
         logoutTest.logout();
+
+        editIssue.changeSummaryField();
+        editIssue.changeIssueType();
+        editIssue.changePriorityField();
+        editIssue.changeLabelField();
+        editIssue.changeDescriptionField();
+        editIssue.addNewAttachment();
+        editIssue.addNewComment();
     }
 }
