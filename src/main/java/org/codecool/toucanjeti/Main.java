ca0728007ef88d38bd934ce22d3531440448a1ad
csege <cseloteigergely@gmail.com>
2023-11-20 11:47:57 +0100

    refactor LogoutTest to LogoutPOM

--- src/main/java/org/codecool/toucanjeti/Main.java
+++ src/main/java/org/codecool/toucanjeti/Main.java
@@ -2,7 +2,7 @@ package org.codecool.toucanjeti;
 
 import org.codecool.toucanjeti.BrowseIssue.BrowseIssuesTest;
 import org.codecool.toucanjeti.Login.LoginTest;
-import org.codecool.toucanjeti.Logout.LogoutTest;
+import org.codecool.toucanjeti.Logout.LogoutPOM;
 import org.codecool.toucanjeti.editIssue.EditIssue;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -12,7 +12,7 @@ public class Main {
 
         WebDriver driver = new ChromeDriver();
         LoginTest loginTest = new LoginTest(driver);
-        LogoutTest logoutTest = new LogoutTest(driver);
+        LogoutPOM logoutPOM = new LogoutPOM(driver);
         EditIssue editIssue = new EditIssue(driver);
 
         editIssue.changeSummaryField();
