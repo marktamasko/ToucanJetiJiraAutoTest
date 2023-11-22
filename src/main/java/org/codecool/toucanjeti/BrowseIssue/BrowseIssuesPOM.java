6f84081b4b10019a18073412742c7fd384af3211
csege <cseloteigergely@gmail.com>
2023-11-22 11:13:26 +0100

    add filterIssuesByProject method to ProwseIssuesPOM

--- src/main/java/org/codecool/toucanjeti/BrowseIssue/BrowseIssuesPOM.java
+++ src/main/java/org/codecool/toucanjeti/BrowseIssue/BrowseIssuesPOM.java
@@ -6,7 +6,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class BrowseIssuesPOM {
     private WebDriver driver;
@@ -16,8 +19,8 @@ public class BrowseIssuesPOM {
     //pagination
     @FindBy(className = "aui-iconfont-chevron-right")
     private WebElement paginationNextButton;
-    @FindBy(className = "aui-iconfont-chevron-left")
-    private WebElement paginationPrevButton;
+//    @FindBy(className = "aui-iconfont-chevron-left")
+//    private WebElement paginationPrevButton;
     @FindBy(xpath = "//div[@data-id='project']")
     private WebElement projectFilter;
     @FindBy(id = "searcher-pid-input")
@@ -31,19 +34,37 @@ public class BrowseIssuesPOM {
     private WebElement issueType;
     @FindBy(id = "summary-val")
     private WebElement issueTitle;
+    //Issue IDs
+    @FindBy(className = "issue-link-key")
+    private List<WebElement> issueKeys;
 
     public BrowseIssuesPOM(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
     //TODO method search for title, return with list of title, project and type
     //TODO method for search result exist
     public boolean searchResultExist() {
         return missingResultDiv.isDisplayed();
     }
+
     public List<String> findIssueWithSearchbar(String keyword) {
         searchbar.sendKeys(keyword);
         searchbar.sendKeys(Keys.ENTER);
         return List.of(issueTitle.getText(), issueProjectName.getText(), issueType.getText());
+    }
+
+    public boolean filterIssuesByProject(String projectName) {
+        projectFilter.click();
+        projectFilterSearchbar.sendKeys(projectName);
+        projectFilterSearchbar.sendKeys(Keys.ENTER);
+
+        List<String> issueIDs = new ArrayList<>();
+        while (paginationNextButton.isDisplayed()) {
+            issueIDs.addAll(issueKeys.stream().map(WebElement::getText).toList());
+            paginationNextButton.click();
+        }
+        return issueIDs.stream().filter(id -> !id.contains(projectName)).toList().size() > 0;
     }
 }
