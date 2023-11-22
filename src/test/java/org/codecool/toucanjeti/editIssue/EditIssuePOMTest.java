79b6ccce01b5fa7a500a19f2f7fb890fa87ee02e
marktamasko <25462245+marktamasko@users.noreply.github.com>
2023-11-22 09:22:23 +0100

    fix[EditIssuePOMTest]: create test class for the EditIssuePOM

---
+++ src/test/java/org/codecool/toucanjeti/editIssue/EditIssuePOMTest.java
@@ -0,0 +1,42 @@
+package org.codecool.toucanjeti.editIssue;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class EditIssuePOMTest {
+
+    String issueUrl = "https://jira-auto.codecool.metastage.net/browse/MTP-4389";
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void changeSummaryField() {
+    }
+
+    @Test
+    void changeIssueType() {
+    }
+
+    @Test
+    void changeDescription() {
+    }
+
+    @Test
+    void changePriority() {
+    }
+
+    @Test
+    void changeLabel() {
+    }
+
+    @Test
+    void addNewComment() {
+    }
+}
