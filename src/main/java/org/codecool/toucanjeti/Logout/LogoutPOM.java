39e1ef3e890cc063473db76289d87445a5411ede
csege <cseloteigergely@gmail.com>
2023-11-22 14:30:12 +0100

    add wait to LogoutPOM

--- src/main/java/org/codecool/toucanjeti/Logout/LogoutPOM.java
+++ src/main/java/org/codecool/toucanjeti/Logout/LogoutPOM.java
@@ -6,9 +6,14 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
 
 public class LogoutPOM {
-    private WebDriver driver;
+    private final WebDriver driver;
+    private final WebDriverWait wait;
     @FindBy(id = "header-details-user-fullname")
     private WebElement avatar;
     @FindBy(id = "log_out")
@@ -16,10 +21,12 @@ public class LogoutPOM {
 
     public LogoutPOM(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         PageFactory.initElements(driver,this);
     }
     public boolean logout() {
         avatar.click();
+        wait.until(ExpectedConditions.elementToBeClickable(logoutButton));
         logoutButton.click();
         return driver.getTitle().equals("Logout - Jira Auto");
     }
