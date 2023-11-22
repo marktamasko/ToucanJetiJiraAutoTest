090d1e89afc23d969c0ebdf1f923321a042a863b
csege <cseloteigergely@gmail.com>
2023-11-22 11:15:21 +0100

    add empty test class to BrowseIssuesPOM

---
+++ src/test/java/org/codecool/toucanjeti/BrowseIssue/BrowseIssuesPOMTest.java
@@ -0,0 +1,20 @@
+package org.codecool.toucanjeti.BrowseIssue;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BrowseIssuesPOMTest {
+
+    @Test
+    void searchResultExistTest() {
+    }
+
+    @Test
+    void findIssueWithSearchbarTest() {
+    }
+
+    @Test
+    void filterIssuesByProjectTest() {
+    }
+}
