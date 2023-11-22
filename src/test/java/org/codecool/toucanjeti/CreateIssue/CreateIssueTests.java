ab7ea98fc12bf17beaa80202c78901da5c1d7f32
arpdkvcs <arpdkvcs@gmail.com>
2023-11-22 09:28:00 +0100

    Create CreateIssueTests class

---
+++ src/test/java/org/codecool/toucanjeti/CreateIssue/CreateIssueTests.java
@@ -0,0 +1,14 @@
+package org.codecool.toucanjeti.CreateIssue;
+
+import org.codecool.toucanjeti.createissue.CreateIssuePOM;
+import org.codecool.toucanjeti.mainPage.MainPagePOM;
+import org.openqa.selenium.WebDriver;
+
+public class CreateIssueTests {
+    private WebDriver driver;
+    private MainPagePOM mainPage;
+
+    private CreateIssuePOM createIssue;
+
+
+}
